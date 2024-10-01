9ac923c58e816ef1cf20dceefd8ef77a34909f85
Saima <bareensaima2512@gmail.com>
2024-10-01 16:41:08 -0400

    handling HTTP POST request and storing in-memory json

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 const port = 3000;
 
 // Initializing the request counters for GET and POST
+let products=[];
 let getRequestCount = 0;
 let postRequestCount = 0;
 
@@ -16,20 +17,29 @@ app.get('/products', (req, res) => {
     getRequestCount++;
     console.log('> /products GET: received request')
 
-    res.send("hello"); // Placeholder for response
+    res.json(products); // Placeholder for response
 
     console.log(`< /products GET: sending response`);
     console.log(`Processed request count -- GET: ${getRequestCount}, POST: ${postRequestCount}`);
 })
 
 app.post('/products', (req, res) => {
+    const {productID, name, price, quantity} = req.body;
     postRequestCount++;
-    console.log('> /products POST: received request')
 
-    res.send("hello"); // Placeholder for response
+    // Add the new product to the products array
+    const newProduct = { productID, name, price, quantity };
+    products.push(newProduct);  // Store the product in memory
+
+    console.log('> /products POST: received request')
+    console.log(name);
+  
+    // Send a response indicating the product was added
+    res.status(201).json({ message: 'Product added successfully', product: newProduct });
 
     console.log(`< /products POST: sending response`);
     console.log(`Processed request count -- GET: ${getRequestCount}, POST: ${postRequestCount}`);
+
 })
 
 // Logging request and response information for POST
